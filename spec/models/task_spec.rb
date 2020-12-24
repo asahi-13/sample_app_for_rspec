147f60f3a67f70baad3a31fe7e0b7f11590208b8
asahi-13 <k0003hina2@gmail.com>
2020-12-25 04:43:48 +0900

    [add]titleのバリデーションのテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe Task, type: :model do
   describe 'validation' do
     it '全ての属性が有効であること' do
       task = build(:task)
-      expect(task).to be_valid 
+      expect(task).to be_valid
+    end
+
+    it 'タイトルがない場合無効となること' do
+      task = build(:task, title:"")
+      expect(task).to be_invalid
     end
   end
 end
