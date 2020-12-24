0a42fea7b919f817f63ef41091b23c1ae060c3c7
asahi-13 <k0003hina2@gmail.com>
2020-12-25 04:27:01 +0900

    [add]taskモデルのテストデータ作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    title {"test"}
+    content {"test"}
+    status { "todo" }
+    deadline { Time.now }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it '全ての属性が有効であること' do
+      task = build(:task)
+      expect(task).to be_valid 
+    end
+  end
 end
