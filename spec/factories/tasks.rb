da0d7a564b6d501c1c51e59f5832d8a377547442
asahi-13 <k0003hina2@gmail.com>
2020-12-25 13:54:31 +0900

    [fix]taskのテストデータを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
-    title {"test"}
-    content {"test"}
-    status { "todo" }
-    deadline { Time.now }
+    sequence(:title, "title_1")
+    content { "content" }
+    status { :todo }
+    deadline { 1.week.from_now }
     association :user
   end
 end
