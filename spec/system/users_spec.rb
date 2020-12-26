310566dc5bd2a5be0f84fb79114ee9833d0cfc86
asahi-13 <k0003hina2@gmail.com>
2020-12-27 05:46:39 +0900

    [add]user新規作成成功時のテストを作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン前', focus: true do
+    context 'フォームの入力値が正常' do
+      it 'ユーザーの新規作成が成功する' do
+        visit '/users/new'
+        expect {
+          fill_in 'Email', with: 'example@example.com'
+          fill_in 'Password', with: '12345678'
+          fill_in 'Password confirmation', with: '12345678'
+          click_button 'SignUp'
+        }.to change { User.count }.by(1)
+        expect(page).to have_content('User was successfully created.')
+      end
+    end
+  end
 end
