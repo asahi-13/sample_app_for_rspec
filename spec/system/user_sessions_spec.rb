09591ec3ccaca71bfb667882848443a8d960a007
asahi-13 <k0003hina2@gmail.com>
2020-12-28 04:48:37 +0900

    [add]ログイン成功時のテストを追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
+  describe 'ログイン前', focus: true do
+    context 'フォームの入力値が正常' do
+      it 'ログインが成功する' do
+        user = create(:user)
+        visit '/login'
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        expect(current_path).to eq '/'
+        expect(page).to have_content('Login successful')
+      end
+    end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
