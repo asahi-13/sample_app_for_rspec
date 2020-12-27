0a45575d85f29974b344f0cbad7a905fbe272882
asahi-13 <k0003hina2@gmail.com>
2020-12-28 05:15:51 +0900

    [add]ログアウトのテストを追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  describe 'ログイン前', focus: true do
+  let(:user) {create(:user)}
+
+  describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログインが成功する' do
-        user = create(:user)
         visit '/login'
         fill_in 'Email', with: user.email
         fill_in 'Password', with: 'password'
@@ -16,11 +17,24 @@ RSpec.describe "UserSessions", type: :system do
 
     context 'フォームが未入力' do
       it 'ログイン処理が失敗する' do
-        user = create(:user)
         visit '/login'
         click_button 'Login'
         expect(current_path).to eq '/login'
         expect(page).to have_content('Login failed')
+      end
+    end
+  end
+
+  describe ' ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する' do
+        visit '/login'
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        click_on 'Logout'
+        expect(current_path).to eq '/'
+        expect(page).to have_content('Logged out')
       end
     end
   end
