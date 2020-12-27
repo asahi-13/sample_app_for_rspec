a33f05ff816dae10df2705a100126f5d81041a64
asahi-13 <k0003hina2@gmail.com>
2020-12-27 13:28:36 +0900

    [add]メールアドレス未入力時のテストケースを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -14,5 +14,16 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_content('User was successfully created.')
       end
     end
+
+    context 'メールアドレスが未入力' do
+      it 'ユーザーの新規作成が失敗する' do
+        visit '/users/new'
+        expect {
+          fill_in 'Password', with: '12345678'
+          fill_in 'Password confirmation', with: '12345678'
+          click_button 'SignUp'
+        }.to change { User.count }.by(0)
+      end
+    end
   end
 end
