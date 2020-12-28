3fc37251a89b0abf34d11f867eed09407ce3fde7
asahi-13 <k0003hina2@gmail.com>
2020-12-29 05:38:39 +0900

    [fix]loginメソッドを１つに修正
    引数でユーザーを渡す為、でメソッド内でユーザーを作成する必要がなかったので最初に作成したメソッドを削除

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,13 +1,5 @@
 module LoginHelper
-  def login
-    user = create(:user)
-    visit '/login'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Login'
-  end
-
-  def login_user(user)
+  def login(user)
     visit '/login'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password'
