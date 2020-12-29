d7344b2c5bf5987ce78373cbfdecf3db47e2bf98
asahi-13 <k0003hina2@gmail.com>
2020-12-29 13:56:47 +0900

    [add]ログイン前はタスクの新規作成ページのアクセスが失敗するテストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
+  context 'ログイン前' do
+    it '新規作成ページのアクセスが失敗すること' do
+      visit new_task_path
+      expect(current_path).to eq(login_path)
+    end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
