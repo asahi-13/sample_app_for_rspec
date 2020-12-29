af21e3ad73fa8c3af3cd65a396f97c097374dce5
asahi-13 <k0003hina2@gmail.com>
2020-12-29 14:17:26 +0900

    [add]タスクの新規作成成功のテストケースを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -7,4 +7,27 @@ RSpec.describe "Tasks", type: :system do
       expect(current_path).to eq(login_path)
     end
   end
+
+  describe 'ログイン後' do
+    let(:user){create(:user)}
+
+    before do
+      login(user)
+    end
+
+    context 'フォームの入力値が正常' do
+      it 'タスクの作成が成功' do
+        visit new_task_path
+        expect{
+          fill_in 'Title', with: 'タイトル'
+          fill_in 'Content', with: 'コンテンツ'
+          select 'todo', from: 'Status'
+          fill_in 'Deadline',with: 1.week.from_now
+          click_on 'Create Task'
+        }.to change{Task.count}.by(1)
+        expect(current_path).to eq(task_path(Task.first))
+        expect(page).to have_content('Task was successfully created.')
+      end
+    end
+  end
 end
